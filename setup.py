10f2e0100aa70f7294167b3217f9f1fd9cb519b2
RedFantom <redfantom@outlook.com>
2017-06-27 15:21:30 +0200

    Setup repository as local packaging environment

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='mttkinter',
+    packages=['mttkinter'],
+    version='0.5.0',
+    description='A thread-safe wrapper around Tkinter for Python 2.x by Allen B. Taylor, published by RedFantom',
+    author='Allen B. Taylor',
+    author_email='redfantom@outlook.com',
+    url='https://github.com/RedFantom/mtTkinter',
+    download_url='https://github.com/RedFantom/mtTkinter/releases',
+    keywords=['tkinter', 'threading'],
+    classifiers=[],
+)
