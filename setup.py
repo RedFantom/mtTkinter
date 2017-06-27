b1f80109988cbcfd3be3466dffd313a1da47da96
RedFantom <redfantom@outlook.com>
2017-06-27 15:28:45 +0200

    Add MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='mttkinter',
     packages=['mttkinter'],
     version='0.5.0',
-    description='A thread-safe wrapper around Tkinter for Python 2.x by Allen B. Taylor, published by RedFantom',
-    author='Allen B. Taylor',
+    description='A thread-safe wrapper around Tkinter for Python 2.x',
+    author='mtTkinter authors',
     author_email='redfantom@outlook.com',
     url='https://github.com/RedFantom/mtTkinter',
     download_url='https://github.com/RedFantom/mtTkinter/releases',
+    license="LGPLv3",
     keywords=['tkinter', 'threading'],
-    classifiers=[],
+    classifiers=["Programming Language :: Python :: 2 :: Only",
+                 "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)"],
+    include_package_data=True
 )
